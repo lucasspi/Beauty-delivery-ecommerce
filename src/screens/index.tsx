4247507ecf2c76841bda8ac9243f7364ff381769
lspirandeli <21196545+lucasspi@users.noreply.github.com>
2021-09-10 09:28:32 -0300

    feat: adding router

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import { Home } from "./home";
-
 import store from "../store";
 
 export const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <Home />
+      <Suspense fallback={<div className="loading" />}>
+        <Router>
+          <Switch>
+            <Route path="/" render={() => <Home />} />
+          </Switch>
+        </Router>
+      </Suspense>
     </Provider>
   );
 };
